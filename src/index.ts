dc152734a4ca5ca27ab211df25bd50584c34eb3b
jovany <jovany.velez@gmail.com>
2024-08-11 00:36:42 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
-import { Elysia } from "elysia";
+import { Elysia,t } from "elysia";
+import { createUser } from './db/queries/insert.js';
+//import db from './db/index.js';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const getFunction = () => {
+  return "Hello";
+}
+
+const app = new Elysia()
+  .get("/", getFunction())
+  .get('/id/:id', ({ params: {id} })=> `Hello ${id}`)
+  .get('/saludo', ()=>{return 'hi';})
+  .get('/personal', ({ error }) => error(404, "Kirifuji Nagisa"))
+  .post('/hi', () => 'hi')
+  .post('/user', ({ body }) => createUser(body), {
+    body: t.Object({
+      name: t.String(),
+      age: t.Number(),
+      email: t.String(),
+    }),
+  })
+  .onError(({code}) => {
+      if(code === 'NOT_FOUND') {
+        return 'Lo siento mucho no encuentro la ruta :(';
+      }
+    }
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+
+
+
+
+
